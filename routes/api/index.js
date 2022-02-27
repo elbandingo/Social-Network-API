f4a192e9d3a5305b2c7065ad734b3a2c6548f71c
el bandingo <elbandingo@protonmail.com>
2022-02-27 16:03:21 -0330

    added the main route files, and created the index.js logic to include the routes, use them, and export the router functionality

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+//require express router, and the user and thought routes in main logic
+const router = require('express').router;
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+
+//use the routes for users and thoughts
+router.use('./users', userRoutes);
+router.use('./thoughts', thoughtRoutes);
+
+//export the module router
+module.exports = router;
