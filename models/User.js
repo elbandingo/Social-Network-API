69f288e825ab83f2aa5eabdec79ec8217b0f1cef
el bandingo <elbandingo@protonmail.com>
2022-02-27 15:45:18 -0330

    added User model, created the schema with email match

---
+++ models/User.js
@@ -0,0 +1,34 @@
+//create a schema require from mongoose
+const { Schema, model } = require('mongoose');
+
+//create a new user schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{type: Schema.Types.ObjectId, ref: 'User'}]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
