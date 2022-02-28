fd3839204904a7eb7d1aa3e3d897e63943747fa7
el bandingo <elbandingo@protonmail.com>
2022-02-27 21:02:10 -0330

    added delete thought, update thought, and fixed bug in thought-routes file

---
+++ controllers/thought-controllers.js
@@ -0,0 +1,120 @@
+const { User, Thought, Reaction} = require('../models');
+const { findOneAndDelete } = require('../models/User');
+
+//create the object for all the routes functions
+const thoughtController = {
+
+//get the thoughts at /api/thoughts
+getAllThoughts(req,res) {
+    Thought.find({})
+    .populate({path: 'reactions', select: '-__v'})
+    .select('-__v')
+    .then(thoughtData => res.json(thoughtData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+},
+
+//get thought by ID at /api/thoughts/:id
+getThoughtById({params}, res) {
+    Thought.findOne({_id: params.id})
+    .populate({path: 'reactions', select: '-__v'})
+    .select('-__v')
+    .then(thoughtData => {
+        if(!thoughtData) {
+            res.status(404).json({message: 'There is no thought with that ID'});
+            return;
+        }
+        res.json(thoughtData);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(400).json(err);
+    }); 
+
+    
+},
+
+
+//post a thought at /api/thoughts
+// example data
+//{
+//    "thoughtText": "Here's a cool thought...",
+//    "username": "lernantino",
+//    "userId": "5edff358a0fcb779aa7b118b"
+//  }
+
+createThought({body}, res) {
+    Thought.create(body)
+    .then(thoughtData => {
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$push: {thoughts:thoughtData._id}},
+            {new:true}
+        )
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({message: `there is no user found with this id: ${params.userId}`});
+                return;
+            }
+            res.json(userData);
+        }).catch(err => res.json(err));
+    }).catch(err => res.json(err));
+},
+
+
+//put request to update a thought at /api/thoughts/:id
+// example data
+//{
+//    "thoughtText": "Here's a cool thought...",
+//    "username": "lernantino",
+//    "userId": "5edff358a0fcb779aa7b118b"
+//  }
+
+updateThought({params, body}, res) {
+    Thought.findOneAndUpdate(
+        {_id: params.id},
+        body,
+        {new:true}
+    )
+    .then(thoughtData => {
+        if(!thoughtData) {
+            res.status(404).json({message: `there is no message with that id number: ${params.id}`});
+            return;
+        }
+        res.json(thoughtData);
+    }).catch(err => res.status(400).json(err));
+},
+
+//delete request to remove a thought based on ID at /api/thoughts/:id
+deleteThought({params}, res) {
+    //delete the thought from the database
+    Thought.findOneAndDelete({_id:params.id})
+    then(thoughtData => {
+        if(!thoughtData) {
+            res.status(404).json({message: `there is no message with that id number: ${params.id}`});
+            return;
+        }
+
+        //delete any refernce  in users thought array
+        User.findOneAndUpdate(
+            {username: thoughtData.username},
+            {$pull: {thoughts: params.id}}
+            )
+            .then(()=>{
+                res.json({message: 'Successfully Removed the thought'});
+            })
+            .catch(err => res.status(500).json(err));
+    })
+    .catch(err => res.status(500).json(err));
+}
+
+
+
+
+
+
+}
+
+module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,36 +1,50 @@
 //require the router function
-//const router = require('express').Router();
+const router = require('express').Router();
 //create an object of functions that will be called on when interacting with the applications routes
-//const {
-    //getAllThoughts,
-    //getThoughtById,
-    //createThought,
-    //updateThought,
-    //deleteThought,
-  //  addReaction,
-//    deleteReaction
-//} = require('../../controllers/thought-controller');
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controllers');
 
 //route options for the main api route, which is getting all thoughts, and creating a thought
 
-//router
-//.route('/')
-//.get(getAllThoughts)
-//.post(createThought);
+router
+.route('/')
+.get(getAllThoughts, (req,res) => {
+  res.json();
+})
+.post(createThought, (req,res) => {
+  res.json()
+});
 
 //route options for anything based on a thoughts ID
-//router
-//.route("/:id")
-//.get(getThoughtById)
-//.put(updateThought)
-//.delete(deleteThought);
+router
+.route("/:id")
+.get(getThoughtById,(req,res) => {
+  res.json();
+})
+.put(updateThought, (req,res) => {
+  res.json()
+})
+.delete(deleteThought, (req,res) => {
+  res.json()
+});
 
 //route options or reactions
 
-//router
-//.route('/:thoughtId/reactions/')
-//.post(addReaction)
-//.delete(deleteReaction);
+router
+.route('/:thoughtId/reactions/')
+.post(addReaction, (req,res) => {
+  res.json()
+})
+.delete(deleteReaction, (req,res) => {
+  res.json()
+});
 
 //export the router
-//module.exports = router;
+module.exports = router;
